f421d789ff6e41974be787851b9c81de4f45dc5b
Stasi-Goreng <fabianaurich@gmail.com>
2026-04-15 11:29:03 +0200

    Optimized quantity input field in AddItemForm for both gorcery and favorite templates.

--- app/favorites/forms.py
+++ app/favorites/forms.py
@@ -1,14 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import DecimalField, StringField, SubmitField
+from wtforms import IntegerField, StringField, SubmitField
 from wtforms.validators import DataRequired, NumberRange, Optional
 
 
 class AddFavoriteForm(FlaskForm):
     name = StringField("Item Name", validators=[DataRequired()])
-    quantity = DecimalField(
+    quantity = IntegerField(
         "Default Quantity",
         default=1,
-        validators=[Optional(), NumberRange(min=0.01)],
+        validators=[Optional(), NumberRange(min=1, max=9999)],
     )
     unit = StringField("Default Unit", validators=[Optional()])
     note = StringField("Default Note", validators=[Optional()])
